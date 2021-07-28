6d2d48eed5885c98437c66839195bdf24be73f67
fox0430 <ipoduser0430@gmail.com>
2021-07-28 19:31:48 +0900

    Rename: seccomp -> libseccomp

--- src/pkgs/container.nim
+++ src/pkgs/container.nim
@@ -1,6 +1,6 @@
 import os, oids, strformat, json, osproc, posix, inotify, strutils
 import image, linuxutils, settings, cgroups
-import seccomp/seccomp
+import libseccomp/seccomp
 
 type State = enum
   running
