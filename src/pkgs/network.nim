a796bab426fb2157a8d82b651d29d93d9c248f67
fox0430 <ipoduser0430@gmail.com>
2021-11-07 02:27:45 +0900

    WIP: Fix networking

--- src/pkgs/network.nim
+++ src/pkgs/network.nim
@@ -1,6 +1,20 @@
 import posix, strformat, os, strutils, osproc
 import linuxutils
 
+proc getActualInterfaceName(interfaceName: string): string =
+  if interfaceName.len < 1:
+    exception("Invalid interface name")
+
+  let r = execCmdEx("ip a")
+
+  if r.exitCode == 0:
+    let lines = r.output.splitLines
+    for l in lines:
+      if l.contains(interfaceName):
+        let splited = l.split(":")
+        # TODO: Add error handling
+        result = splited[1].splitWhitespace[0]
+
 proc upNetworkInterface*(interfaceName: string) =
   let
     cmd = fmt"ip link set {interfaceName} up"
@@ -14,13 +28,14 @@ proc createVirtualEthnet*(hostInterfaceName, containerInterfaceName: string) =
     cmd = fmt"ip link add name {hostInterfaceName} type veth peer name {containerInterfaceName}"
     r = execShellCmd(cmd)
 
-  if r != 0:
+  if r != 0 and r != 2:
     exception(fmt"Failed to '{cmd}': exitCode: {r}")
 
 # TODO: Add type for IP address
 proc addIpAddrToVeth*(interfaceName, ipAddr: string) =
   let
-    cmd = fmt"ip addr add {ipAddr} dev {interfaceName}"
+    actualInterfaceName = getActualInterfaceName(interfaceName)
+    cmd = fmt"ip addr add {ipAddr} dev {actualInterfaceName}"
     r = execShellCmd(cmd)
 
   if r != 0:
@@ -28,14 +43,10 @@ proc addIpAddrToVeth*(interfaceName, ipAddr: string) =
 
 # Wait for a network interface to be ready.
 proc waitInterfaceReady*(interfaceName: string) =
-  let
-    r = execCmdEx("ip a")
-    exitCode = r.exitCode
-
-  if exitCode == 0:
-    let output = $r.output
+  let r = execCmdEx("ip a")
+  if r.exitCode == 0:
     while true:
-      if output.contains(interfaceName):
+      if r.output.contains(interfaceName):
         break
   else:
     exception("Failed to ip command in container")
@@ -50,22 +61,27 @@ proc addInterfaceToContainer*(containerInterfaceName: string, pid: Pid) =
       exception(fmt"Failed to '{cmd}': exitCode: {r}")
 
   block:
+    # TODO: Need to get actial interface name from new netns
+    let actualInterfaceName = getActualInterfaceName(containerInterfaceName)
+
     # TODO: Fix IP
     const IP_ADDR = "10.0.0.1/24"
-    addIpAddrToVeth(containerInterfaceName, IP_ADDR)
+    addIpAddrToVeth(actualInterfaceName, IP_ADDR)
 
-  upNetworkInterface(containerInterfaceName)
+    upNetworkInterface(actualInterfaceName)
 
 # TODO: Add type for IP address
 proc initContainerNetwork*(
   containerId, hostInterfaceName, containerInterfaceName, ipAddr: string) =
+
+  let actualInterfaceName = getActualInterfaceName(containerInterfaceName)
 
   block:
     const DEVIC_ENAME = "lo"
     upNetworkInterface(DEVIC_ENAME)
 
   # Wait for a network interface to be ready.
-  waitInterfaceReady(containerInterfaceName)
+  waitInterfaceReady(actualInterfaceName)
 
   addIpAddrToVeth(hostInterfaceName, ipAddr)
-  upNetworkInterface(containerInterfaceName)
+  upNetworkInterface(actualInterfaceName)
