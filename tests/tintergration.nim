769fcf1ab79e8a47e20f08dbabbab1a5bad94d07
fox0430 <ipoduser0430@gmail.com>
2021-12-22 04:43:43 +0900

    Add integration tests

--- tests/tintergration.nim
+++ tests/tintergration.nim
@@ -12,8 +12,26 @@ proc getLastLineOfCmdResult(output: string): string =
 suite "Integration":
   test "Run Container":
 
+    # First of all, build Nicoru
     check buildNicoru()
 
     let r = execCmdEx("""sudo ./nicoru run alpine:latest echo "ok"""")
     check r.exitCode == 0
     check getLastLineOfCmdResult(r.output) == "ok"
+
+  test "ping in the container (Host mode)":
+    let r = execCmdEx("""sudo ./nicoru run alpine:latest ping -c 1 google.com""")
+    check r.exitCode == 0
+
+  test "ping in the container (Bridge mode)":
+    let r = execCmdEx("""sudo ./nicoru run --network=bridge alpine:latest ping -c 1 google.com""")
+    check r.exitCode == 0
+
+  test "Show containers":
+    let r = execCmdEx("sudo ./nicoru ps")
+    check r.exitCode == 0
+
+  test "Show images":
+    let r = execCmdEx("sudo ./nicoru images")
+    check r.exitCode == 0
+
